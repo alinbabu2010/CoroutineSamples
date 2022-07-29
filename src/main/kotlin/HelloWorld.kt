2cab486c9dff5c63a7f3a43ac1d43a915c572d03
Alin Babu <alinbabu2010@gmail.com>
2022-07-29 12:08:09 +0530

    Add code to print `Hello, World!` using coroutine

---
+++ src/main/kotlin/HelloWorld.kt
@@ -0,0 +1,17 @@
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@DelicateCoroutinesApi
+fun main() {
+
+    GlobalScope.launch {
+        delay(2000)
+        println("World!")
+    }
+
+    print("Hello, ")
+    Thread.sleep(3000)
+
+}
