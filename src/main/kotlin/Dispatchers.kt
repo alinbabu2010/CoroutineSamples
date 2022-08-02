10b5d5b04ac2b3c33c194f95d175d65c0630f236
Alin Babu <alinbabu2010@gmail.com>
2022-08-02 09:09:15 +0530

    Demo code showing coroutine dispatchers

---
+++ src/main/kotlin/Dispatchers.kt
@@ -0,0 +1,30 @@
+import kotlinx.coroutines.*
+
+@DelicateCoroutinesApi
+fun main() {
+    runBlocking {
+
+        /*launch(Dispatchers.Main) {
+            println("Main dispatcher. Thread: ${Thread.currentThread().name}")
+        }*/
+
+        launch(Dispatchers.Unconfined) {
+            println("Unconfined dispatcher. Thread: ${Thread.currentThread().name}")
+            delay(100L)
+            println("Unconfined2. Thread: ${Thread.currentThread().name}")
+        }
+
+        launch(Dispatchers.Default) {
+            println("Default dispatcher. Thread: ${Thread.currentThread().name}")
+        }
+
+        launch(Dispatchers.IO) {
+            println("IO dispatcher. Thread: ${Thread.currentThread().name}")
+        }
+
+        launch(newSingleThreadContext("MyThread")) {
+            println("newSingleThreadContext. Thread: ${Thread.currentThread().name}")
+        }
+
+    }
+}
