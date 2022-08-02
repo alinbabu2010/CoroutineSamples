ccd1360d68c2cf391c34b7bf6e95dfff0ec5a24f
Alin Babu <alinbabu2010@gmail.com>
2022-08-02 09:41:16 +0530

    Demo code showing coroutine exception handling

---
+++ src/main/kotlin/Exceptions.kt
@@ -0,0 +1,32 @@
+import kotlinx.coroutines.*
+
+@DelicateCoroutinesApi
+fun main() {
+
+    runBlocking {
+
+        val myHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+            println("Exception handled: ${throwable.localizedMessage}")
+        }
+
+        val job = GlobalScope.launch(myHandler) {
+            println("Throwing exception from job")
+            throw IndexOutOfBoundsException("Exception in coroutine")
+        }
+
+        job.join()
+
+        val deferred = GlobalScope.async {
+            println("Throwing exception from async")
+            throw ArithmeticException("exception form async")
+        }
+
+        try {
+            deferred.await()
+        } catch (e: ArithmeticException) {
+            println("Caught ArithmeticException ${e.localizedMessage}")
+        }
+
+    }
+
+}
