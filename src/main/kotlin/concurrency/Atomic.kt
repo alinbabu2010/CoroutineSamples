89bf6278bb7a90b45d6b3a54f218f2124e444998
Alin Babu <alinbabu2010@gmail.com>
2022-08-05 14:53:47 +0530

    Demo code showing atomic variable usage

---
+++ src/main/kotlin/concurrency/Atomic.kt
@@ -0,0 +1,32 @@
+package concurrency
+
+import kotlinx.coroutines.*
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    runBlocking {
+        val counter = AtomicInteger(0)
+        withContext(Dispatchers.Default) {
+            massiveRun { counter.incrementAndGet() }
+        }
+        println("Counter = $counter")
+    }
+}
+
+private suspend fun massiveRun(action: suspend () -> Unit) {
+    val n = 100
+    val k = 1000
+    val time = measureTimeMillis {
+        coroutineScope {
+            repeat(n) {
+                launch {
+                    repeat(k) {
+                        action()
+                    }
+                }
+            }
+        }
+    }
+    println("Completed ${n * k} actions in $time ms")
+}
